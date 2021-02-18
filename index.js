a4f4c3d1fefa2eeff77840b28d716eb5d8662032
Justin Douville <jdouville8@gmail.com>
2021-02-17 20:25:47 -0500

    createed classes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Require npm package jest and inquirer as well as classes.js for any classes used
+const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+
+const Manager1 = new Intern('Ben', 42, 'ben@ben', 'CU');
+
+console.log(Manager1.getSchool());
